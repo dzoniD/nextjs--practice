43f48712d1b27d48811d07ac31df520efd3c834c
dzoniD <edvard_dantes@hotmail.com>
2020-04-29 15:37:17 +0200

    setting up a project

---
+++ .next/static/development/pages/auth.js
@@ -0,0 +1,103 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static\\development\\pages\\auth.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fauth&absolutePagePath=C%3A%5CUsers%5CNikola%5CDesktop%5Cradovi%5CUdemy%20Kurs%5CSection%2023-Next.js%5Cpages%5Cauth%5Cindex.js&hotRouterUpdates=true!./":
+/*!***********************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fauth&absolutePagePath=C%3A%5CUsers%5CNikola%5CDesktop%5Cradovi%5CUdemy%20Kurs%5CSection%2023-Next.js%5Cpages%5Cauth%5Cindex.js&hotRouterUpdates=true ***!
+  \***********************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P = window.__NEXT_P || []).push([
+      "/auth",
+      function () {
+        var mod = __webpack_require__(/*! ./pages/auth/index.js */ "./pages/auth/index.js");
+        if (true) {
+          module.hot.accept(/*! ./pages/auth/index.js */ "./pages/auth/index.js", function () {
+            if (!next.router.components["/auth"]) return;
+            var updatedPage = __webpack_require__(/*! ./pages/auth/index.js */ "./pages/auth/index.js");
+            next.router.update("/auth", updatedPage);
+          });
+        }
+        return mod;
+      }
+    ]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_c2e10d183b950a67d9e7 ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_c2e10d183b950a67d9e7 */ "dll-reference dll_c2e10d183b950a67d9e7"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/auth/index.js":
+/*!*****************************!*\
+  !*** ./pages/auth/index.js ***!
+  \*****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _this = undefined,
+    _jsxFileName = "C:\\Users\\Nikola\\Desktop\\radovi\\Udemy Kurs\\Section 23-Next.js\\pages\\auth\\index.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+var authIndexPage = function authIndexPage() {
+  return __jsx("div", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4,
+      columnNumber: 5
+    }
+  }, __jsx("h1", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5,
+      columnNumber: 9
+    }
+  }, "The Auth Index Page"));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (authIndexPage);
+
+/***/ }),
+
+/***/ 2:
+/*!***************************************************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fauth&absolutePagePath=C%3A%5CUsers%5CNikola%5CDesktop%5Cradovi%5CUdemy%20Kurs%5CSection%2023-Next.js%5Cpages%5Cauth%5Cindex.js&hotRouterUpdates=true ***!
+  \***************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fauth&absolutePagePath=C%3A%5CUsers%5CNikola%5CDesktop%5Cradovi%5CUdemy%20Kurs%5CSection%2023-Next.js%5Cpages%5Cauth%5Cindex.js&hotRouterUpdates=true! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fauth&absolutePagePath=C%3A%5CUsers%5CNikola%5CDesktop%5Cradovi%5CUdemy%20Kurs%5CSection%2023-Next.js%5Cpages%5Cauth%5Cindex.js&hotRouterUpdates=true!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_c2e10d183b950a67d9e7":
+/*!*******************************************!*\
+  !*** external "dll_c2e10d183b950a67d9e7" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_c2e10d183b950a67d9e7;
+
+/***/ })
+
+},[[2,"static/runtime/webpack.js"]]]);
+//# sourceMappingURL=auth.js.map
