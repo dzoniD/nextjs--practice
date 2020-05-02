e61f51de7518071f34fe294aa4b70e1ad4657741
dzoniD <edvard_dantes@hotmail.com>
2020-05-02 15:43:04 +0200

    using getInitialProps

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "242a264f88502c845e2c";
+/******/ 	var hotCurrentHash = "7dfdb165fb3db8a35f42";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
